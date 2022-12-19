7de90714338a2578928da291cc550ac292948a85
Karthikeya <80380800+karthikeyay@users.noreply.github.com>
2022-12-20 00:15:37 +0530

    cors debug

--- middlewares/customMiddleware.js
+++ middlewares/customMiddleware.js
@@ -3,6 +3,7 @@ module.exports = strapi => {
         initialize() {
             strapi.app.use(async (ctx, next) => {
                 ctx.response.set('Access-Control-Allow-Origin', '*');
+                console.log(ctx.request.url , "CORS Header Set");
                 await next();
             });
         },
