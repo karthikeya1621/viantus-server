bb750a26bcb07b160829cdb4a91e37b66858a755
Karthikeya <80380800+karthikeyay@users.noreply.github.com>
2022-12-20 02:06:17 +0530

    cors fix7

--- api/about-us/controllers/about-us.js
+++ api/about-us/controllers/about-us.js
@@ -3,8 +3,8 @@ const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
 
   async find(ctx) {
-    const entity = await strapi.services.aboutUs.find();
+    const entity = await strapi.services['about-us'].find();
     ctx.set('Access-Control-Allow-Origin', '*');
-    return sanitizeEntity(entity, { model: strapi.models.aboutUs });
+    return sanitizeEntity(entity, { model: strapi.models['about-us'] });
   },
 };
--- api/home-page/controllers/home-page.js
+++ api/home-page/controllers/home-page.js
@@ -3,8 +3,8 @@ const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
 
   async find(ctx) {
-    const entity = await strapi.services.homePage.find();
+    const entity = await strapi.services['home-page'].find();
     ctx.set('Access-Control-Allow-Origin', '*');
-    return sanitizeEntity(entity, { model: strapi.models.homePage });
+    return sanitizeEntity(entity, { model: strapi.models['home-page'] });
   },
 };
