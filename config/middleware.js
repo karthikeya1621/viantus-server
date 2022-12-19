eacf47462a76a48c66753da1612c242dd447ca41
Karthikeya <80380800+karthikeyay@users.noreply.github.com>
2022-12-20 01:26:45 +0530

    cors fix5

--- config/middleware.js
+++ config/middleware.js
@@ -1,11 +1,7 @@
 module.exports = {
-    settings: {
-      cors: {
-        enabled: true,
-        origin: ["http://localhost", "https://viantus.com", "https://www.viantus.com", "http://viantus.com", "http://www.viantus.com"]
-      },
-      customMiddleware: {
-        enabled: true
-      }
-    },
-  };
+  settings: {
+    cors: {
+      enabled: true
+    }
+  },
+};
